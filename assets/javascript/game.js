54c063a20327ec17f0f18bbefc19b8ee64a36b08
Jim <jimmybnguyen@yahoo.com>
2019-10-12 20:31:09 -0700

    game not resetting after win/lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,13 +14,10 @@ var wins= 0;
 var losses= 0;
 var score= 0;
 
-
 $('#randomNumber').html(random);
 $('.wins').html(wins);
 $('.losses').html(losses);
 $('#score').html(score);
-
-
 
 function reset() {
 random=Math.floor(Math.random() * 101 + 19);
@@ -37,19 +34,18 @@ console.log(btnSecondary3);
 btnSecondary4= Math.floor(Math.random() * 11 + 1);
 console.log(btnSecondary4);
 
-
 function winning(){
     alert("You win!");
     wins++;
     $('.wins').html(wins);
-    
+    reset('#score');
 }
 
 function losing() {
     alert("You lose!");
     losses++;
     $('.losses').html(losses);
-    
+    reset('#score');
 }
 
 $('.btnSecondary1').on('click', function() {
